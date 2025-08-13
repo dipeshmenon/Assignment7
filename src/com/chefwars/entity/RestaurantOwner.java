c800fd132a90586a9975a5639eab5d863124c4b6
dipeshmenon <dipesh.menon3003@gmail.com>
2025-08-13 09:42:15 +0530

    Changes

--- src/com/chefwars/entity/RestaurantOwner.java
+++ src/com/chefwars/entity/RestaurantOwner.java
@@ -14,11 +14,11 @@ public class RestaurantOwner extends User {
     }
 
     public void addDish(String dishName, double price) {
-        System.out.println("Added dish: " + dishName + " at price $" + price);
+        System.out.println("Added dish: " + dishName + " at price: " + price);
     }
 
     public void changeDishPrice(String dishName, double newPrice) {
-        System.out.println("Changed price of " + dishName + " to $" + newPrice);
+        System.out.println("Changed price of " + dishName + " to " + newPrice);
     }
 
     public void runOffer(String offerDetails) {
